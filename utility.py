17a599313fc0b35a75902a4278e8f2b37441651e
Jack Klamer <jfklamer@gmail.com>
2017-04-26 01:17:58 -0400

    New Python code more object development

--- utility.py
+++ utility.py
@@ -1,8 +1,6 @@
 #Utility objects and wrappers for the project
-#Jack Klamer 
+#Jack Klamer
 import xml.sax as xml
-
-
 
 class ElementHandler(xml.ContentHandler):
 	def __init__(self):
@@ -18,27 +16,65 @@ class ElementHandler(xml.ContentHandler):
 	def characters(self, content):
 		pass
 
+class OsmMap:
+	def __init__(self):
+		self.nodes = dict()
+		self.ways = dict()
+		self.relations= dict()
+
+	def addNode(self, node):
+		if not isinstance(OsmNode, node):
+			raise Exception("Must add OsmNode as Node")
+		self.nodes[node.id]=node
+
+	def addWay(self, way):
+		if not isinstance(OsmWay, way):
+			raise Exception("Must add OsmWay as way")
+		self.way[way.id]=way
+
+	def addRelation(self, relation):
+		if not isinstance(OsmRelation, relation):
+			raise Exception("Must add OsmRelation as Relation")
+		self.relations[relation.id]=relation
+
+
 
 class OsmNode:
 	def __init__(self, id, lat, lon):
-		self.id = id
-		self.lat = lat 
-		self.long = lon
+		self.id = int(id)
+		self.lat = float(lat)
+		self.long = float(lon)
 		self.tags = dict()
 
 	def getCoord(self):
 		return self.lat, self.lon
+
 	def __hash__(self):
 		return hash(self.id)
+
 	def __eq__(self, other):
 		return self.id ==  other.id
 
 class OsmWay:
 	def __init__(self, id):
-		self.id = id
+		self.id = int(id)
 		self.nodes = list()
 		self.tags = dict()
 
+	def __hash__(self):
+		return hash(self.id)
 
-		
+	def __eq__(self, other):
+		return self.id ==  other.id
 
+class OsmRelation:
+	def __init__(self, id):
+		self.id= int(id)
+		self.members=dict()
+		self.tags=dict()
+
+	def __hash__(self):
+		return hash(self.id)
+
+	def __eq__(self, other):
+		return self.id ==  other.id
