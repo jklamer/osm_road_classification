f2b601b7f9eb14af7e5b54d51212401fc994a6fb
Jack Klamer <jfklamer@gmail.com>
2017-04-27 13:39:57 -0400

    command line args complete

--- map2vec.py
+++ map2vec.py
@@ -3,18 +3,40 @@ import xml.sax as xml
 from xml.etree.ElementTree import Element, SubElement, Comment, tostring
 import datetime
 from osm import *
+usage="USAGE: map2vec.py INPUTS -o OUTPUTS"
 
-
+if len(sys.argv)== 1 or sys.argv[1] == '-h' or sys.argv[1]=='--help':
+	print(usage)
+	exit()
 
 if __name__ == "__main__":
 
-	#init the object
+	inputs=[]
+	outputs=[]
+	#parse inputs
+	parsingInputs= True
+	for arg in sys.argv[1:]:
+		if arg == '-o':
+			parsingInputs=False
+		elif parsingInputs:
+			inputs.append(arg)
+		else:
+			outputs.append(arg)
+
+	if len(inputs) != len(outputs):
+		print(usage)
+		print("Number of outputs must equal number of inputs")
+
+	#init the parsing object
 	MapParser = xml.make_parser()
 	MapParser.setFeature(xml.handler.feature_namespaces, 0)
 
-	#create specific handler for each map
-	map = OsmMap()
-	myHandler = OsmDataContentHandler(map, wayRequireTags=['highway'])
-	MapParser.setContentHandler(myHandler)
-	MapParser.parse(open("data/reykjavik_iceland.osm"))
-	map.printMap(file = open("reykjavik_iceland.txt",'w'))
+	for i in range(len(inputs)):
+		#create specific handler for each map
+		map = OsmMap()
+		myHandler = OsmDataContentHandler(map, wayRequireTags=['highway'])
+		MapParser.setContentHandler(myHandler)
+		MapParser.parse(open(inputs[i]))
+		map.printMap(file = open(outputs[i],'w'))
+
+		del(map)
